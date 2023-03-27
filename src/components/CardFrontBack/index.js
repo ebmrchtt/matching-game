3d904be7952addaa266f4ade4af88c880ba6bcf8
Eber J <eberjr085@gmail.com>
2023-03-27 17:28:56 -0300

    :sparkles: Flip card added

--- src/components/CardFrontBack/index.js
+++ src/components/CardFrontBack/index.js
@@ -1,10 +1,15 @@
 import CardGame from "../CardGame";
+import "./style.css";
 
 function CardFrontBack() {
     return /*html*/`
         <article class="card-front-back">
-            ${CardGame()}
-            ${CardGame("html", "Logo do HTML")}
+            <div class="card -front">
+                ${CardGame()}
+            </div>
+            <div class="card -back">
+                ${CardGame("html", "Logo do HTML")}
+            </div>
         </article>
     `
 }
